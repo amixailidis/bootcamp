792d7758ebaf4e0a889ab0525e834c58c253546b
amixailidis <62053616+amixailidis@users.noreply.github.com>
2020-03-11 11:06:25 +0200

    Add files via upload

---
+++ Mathematics/src/mathematics/Mathematics.java
@@ -0,0 +1,18 @@
+
+package mathematics;
+
+
+public class Mathematics {
+
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int minimum = Math.min(a, b);
+              minimum = Math.min(minimum, c);
+        System.out.println("Minimum of numbers 10 and 20 and 30 is " +minimum);
+                
+                }
+    
+}
